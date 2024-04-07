ed0c8c4764cd58c6ae6f8b7f9066912b93b41421
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2024-04-07 15:14:48 +0300

    Create Job Layout and fetch data
    -create route

---
+++ job_board_app/resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Board</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
--- job_board_app/routes/web.php
+++ job_board_app/routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('jobs', JobController::class)
+    ->only(['index']);
 
 Auth::routes();
 
