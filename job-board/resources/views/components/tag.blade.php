6e7cd69b05fa843753f95b98fdb7899fb20dc091
nvmarzakov <nikolay.marzakov@gmail.com>
2024-07-13 17:20:23 +0300

    update job card content
    - create tag component
    - update routes

---
+++ job-board/resources/views/components/tag.blade.php
@@ -0,0 +1,3 @@
+<div class="border rounded-3 px-2 py-1 ms-2">
+    {{ $slot }}
+</div>
--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -1,7 +1,35 @@
 <x-layout>
     @foreach ($jobs as $job)
         <x-card class="mb-4">
-            {{ $job->title }}
+            <div class="mb-4 d-flex justify-content-between">
+                <h2 class="">{{ $job->title }}</h2>
+                <div class="text-black-50">
+                    €{{ number_format($job->salary, 2) }}
+                </div>
+            </div>
+
+            <div class="mb-4 d-flex justify-content-between align-items-center">
+                <div class="d-flex">
+                    <div class="me-4">
+                       Company Name 
+                    </div>
+                    <div>
+                        {{ $job->location }}
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <x-tag>
+                        {{ Str::ucfirst($job->experience) }}
+                    </x-tag>
+                    <x-tag>
+                        {{ $job->category }}
+                    </x-tag>
+                        
+                    
+                </div>
+            </div>
+
+            <p>{{ nl2br($job->description) }}</p>
         </x-card>
     @endforeach
 </x-layout>
